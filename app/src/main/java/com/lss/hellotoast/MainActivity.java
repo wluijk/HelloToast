5aaebaad268cdc0b9069d2686d4b00065b83ea36
wluijk <wluijk@gmail.com>
2018-05-25 00:22:22 +0200

    Commit vandaag

--- app/src/main/java/com/lss/hellotoast/MainActivity.java
+++ app/src/main/java/com/lss/hellotoast/MainActivity.java
@@ -1,7 +1,9 @@
 package com.lss.hellotoast;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+    }
+
+    public void countUp(View view) {
     }
 }
